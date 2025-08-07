25220e76ce286ce13ab9ead941f799f53be92186
thakurharshit18 <chauhanharshit498@gmail.com>
2025-08-07 21:56:44 +0530

    commit

---
+++ app/Home/page.tsx
@@ -0,0 +1,30 @@
+
+
+export default function Home(){
+
+ 
+
+    return(
+        <>
+         <div className="bg-black">
+            <div>
+                <img src={'./MemeTron'}/>
+            </div>
+            <div className=" relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border  ">
+<span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text  text-9xl font-semibold leading-none tracking-tighter text-transparent absolute top-50 ">
+        Memetron
+      </span>
+      <span className="bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] text-7xl font-bold bg-clip-text tracking-tighter text-transparent relative top-0">
+Type It. Meme It. Share It.
+      </span>
+            </div>
+      
+      
+     
+    </div>
+        
+        
+        
+        </>
+    )
+}
